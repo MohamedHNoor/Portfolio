a97fb36079fc3df509aafc02b8f45630d95e5b5a
Mohamed H Noor <mohmed.hassannoor114@gmail.com>
2022-09-07 22:46:38 +0200

    implements an array of objects to store JavaScript objects

--- js/main.js
+++ js/main.js
@@ -12,79 +12,136 @@ navLinks.forEach((link) => {
 });
 
 // model popup
-const data = {
-  name: "Keeping track of hundreds of components",
-  description:
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-  imageDesktop: "images/desktop-image-portfolio.png",
-  githubIcon: "images/icongithub.svg",
-  liveIcon: "images/icon-export.svg",
-  technologiesMobile: ["Ruby on Rails", "CSS", "JavaScript"],
-  technologiesDesktop: [
-    "CodeKit",
-    "GitHub",
-    "JavaScript",
-    "Bootstrap",
-    "Terminal",
-    "Codepen",
-  ],
-  sourceLink: "https://github.com/MohamedHNoor/mobile-firs-project1",
-  liveLink: "https://mohamedhnoor.github.io/Portfolio-project1/",
-};
+const data = [
+  {
+    id: 1,
+    name: "Keeping track of hundreds of components",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    imageDesktop: "images/desktop-image-portfolio.png",
+    githubIcon: "images/icongithub.svg",
+    liveIcon: "images/icon-export.svg",
+    technologiesMobile: ["Ruby on Rails", "CSS", "JavaScript"],
+    technologiesDesktop: [
+      "CodeKit",
+      "GitHub",
+      "JavaScript",
+      "Bootstrap",
+      "Terminal",
+      "Codepen",
+    ],
+    sourceLink: "https://github.com/MohamedHNoor/mobile-firs-project1",
+    liveLink: "https://mohamedhnoor.github.io/Portfolio-project1/",
+  },
+  {
+    id: 2,
+    name: "Keeping track of hundreds of components",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    imageDesktop: "images/desktop-image-portfolio.png",
+    githubIcon: "images/icongithub.svg",
+    liveIcon: "images/icon-export.svg",
+    technologiesMobile: ["Ruby on Rails", "CSS", "JavaScript"],
+    technologiesDesktop: [
+      "CodeKit",
+      "GitHub",
+      "JavaScript",
+      "Bootstrap",
+      "Terminal",
+      "Codepen",
+    ],
+    sourceLink: "https://github.com/MohamedHNoor/mobile-firs-project1",
+    liveLink: "https://mohamedhnoor.github.io/Portfolio-project1/",
+  },
+  {
+    id: 3,
+    name: "Keeping track of hundreds of components",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    imageDesktop: "images/desktop-image-portfolio.png",
+    githubIcon: "images/icongithub.svg",
+    liveIcon: "images/icon-export.svg",
+    technologiesMobile: ["Ruby on Rails", "CSS", "JavaScript"],
+    technologiesDesktop: [
+      "CodeKit",
+      "GitHub",
+      "JavaScript",
+      "Bootstrap",
+      "Terminal",
+      "Codepen",
+    ],
+    sourceLink: "https://github.com/MohamedHNoor/mobile-firs-project1",
+    liveLink: "https://mohamedhnoor.github.io/Portfolio-project1/",
+  },
+  {
+    id: 4,
+    name: "Keeping track of hundreds of components",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    imageDesktop: "images/desktop-image-portfolio.png",
+    githubIcon: "images/icongithub.svg",
+    liveIcon: "images/icon-export.svg",
+    technologiesMobile: ["Ruby on Rails", "CSS", "JavaScript"],
+    technologiesDesktop: [
+      "CodeKit",
+      "GitHub",
+      "JavaScript",
+      "Bootstrap",
+      "Terminal",
+      "Codepen",
+    ],
+    sourceLink: "https://github.com/MohamedHNoor/mobile-firs-project1",
+    liveLink: "https://mohamedhnoor.github.io/Portfolio-project1/",
+  },
+  {
+    id: 5,
+    name: "Keeping track of hundreds of components",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    imageDesktop: "images/desktop-image-portfolio.png",
+    githubIcon: "images/icongithub.svg",
+    liveIcon: "images/icon-export.svg",
+    technologiesMobile: ["Ruby on Rails", "CSS", "JavaScript"],
+    technologiesDesktop: [
+      "CodeKit",
+      "GitHub",
+      "JavaScript",
+      "Bootstrap",
+      "Terminal",
+      "Codepen",
+    ],
+    sourceLink: "https://github.com/MohamedHNoor/mobile-firs-project1",
+    liveLink: "https://mohamedhnoor.github.io/Portfolio-project1/",
+  },
+  {
+    id: 6,
+    name: "Keeping track of hundreds of components",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    imageDesktop: "images/desktop-image-portfolio.png",
+    githubIcon: "images/icongithub.svg",
+    liveIcon: "images/icon-export.svg",
+    technologiesMobile: ["Ruby on Rails", "CSS", "JavaScript"],
+    technologiesDesktop: [
+      "CodeKit",
+      "GitHub",
+      "JavaScript",
+      "Bootstrap",
+      "Terminal",
+      "Codepen",
+    ],
+    sourceLink: "https://github.com/MohamedHNoor/mobile-firs-project1",
+    liveLink: "https://mohamedhnoor.github.io/Portfolio-project1/",
+  },
+];
+
 const myModal = document.getElementById("myModal");
-myModal.innerHTML = `<div class="modal-content">
-<div class="modal-header">
-  <div class="bg-img">
-  <span class="close">&times;</span>
-  <img class="desktop-bg" src="${data.imageDesktop}" />
-  </div>
-</div>
-<div class="modal-body">
-  <h3 id="title">${data.name}</h3>
-  <ul class="tags" id="tech">
-  ${data.technologiesDesktop.map((ele) => `<li>${ele}</>`)}
-  </ul>
-  <ul class="tags" id="mobile-ul">
-  ${data.technologiesMobile.map((ele) => `<li>${ele}</>`)}
-  </ul>
-  <p id="description">${data.description}</p>
-</div>
-<div class="modal-footer">
-  <a href="${data.liveLink}">
-    <button type="submit" class="project-view-btn">
-      <span> See Live</span>
-      <img
-        src="${data.liveIcon}"
-        alt="See Live Project"
-        width="20"
-        height="20"
-      /></button
-  ></a>
-  <a href="${data.sourceLink}">
-    <button type="submit" class="project-view-btn">
-      <span>See Source</span>
-      <img
-        src="${data.githubIcon}"
-        alt="See Source Code"
-        width="20"
-        height="20"
-      /></button
-  ></a>
-</div>
-</div>
-`;
-
-function viewProjectDetails() {
-  myModal.style.display = "block";
-}
-
 // Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close")[0];
+const span = document.getElementById("closeBtn");
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
+const closeModal = () => {
   myModal.style.display = "none";
-};
+}
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
@@ -92,3 +149,52 @@ window.onclick = function (event) {
     myModal.style.display = "none";
   }
 };
+
+function viewProjectDetails(projectIndex) {
+  const project = data[projectIndex];
+  myModal.innerHTML = `<div class="modal-content">
+<div class="modal-header">
+  <div class="bg-img">
+  <span onclick="closeModal()" id="closeBtn" class="close">&times;</span>
+  <img class="desktop-bg" src="${project.imageDesktop}" />
+  </div>
+</div>
+<div class="modal-body">
+  <div class="title-container">
+    <h3 id="title">${project.name}</h3>
+  </div>
+  <ul class="tags" id="tech">
+  ${project.technologiesDesktop.map((ele) => `<li>${ele}</>`)}
+  </ul>
+  <ul class="tags" id="mobile-ul">
+  ${project.technologiesMobile.map((ele) => `<li>${ele}</>`)}
+  </ul>
+  <p id="description">${project.description}</p>
+  <div class="modal-footer">
+  <a href="${project.liveLink}">
+    <button type="submit" class="project-view-btn">
+      <span> See Live</span>
+      <img
+        src="${project.liveIcon}"
+        alt="See Live Project"
+        width="20"
+        height="20"
+      /></button
+  ></a>
+  <a href="${project.sourceLink}">
+    <button type="submit" class="project-view-btn">
+      <span>See Source</span>
+      <img
+        src="${project.githubIcon}"
+        alt="See Source Code"
+        width="20"
+        height="20"
+      /></button
+  ></a>
+</div>
+</div>
+</div>
+`
+;
+  myModal.style.display = "block";
+}
