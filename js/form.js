21578506ce40f744af6471d79cc542aba8aef1cd
Mohamed H Noor <mohmed.hassannoor114@gmail.com>
2022-09-08 12:54:18 +0200

    add email validation in js file

---
+++ js/form.js
@@ -0,0 +1,22 @@
+const form = document.getElementById('projectForm');
+
+function lowercaseEmail(input) {
+  return input === String(input).toLowerCase();
+}
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  // get email value fromm form
+  const userEmail = form.elements['user-email'].value;
+
+  // check if email is on lowercase
+  const emailLowercase = lowercaseEmail(userEmail);
+
+  // get small element to display error message
+  const small = document.getElementById('small-error');
+
+  if (emailLowercase) {
+    small.innerHTML = '';
+  } else {
+    small.innerHTML = 'Please enter a correct email address format';
+  }
+});
